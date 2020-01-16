dff97dd94b639acf21d76ae4fd59978ecadd99c5
Ed Casillas <eduardo_casillas@hotmail.com>
2020-01-15 18:38:02 -0600

    New fix on Windows.

--- Editor/MissingReferencesFinder.cs
+++ Editor/MissingReferencesFinder.cs
@@ -46,8 +46,7 @@ public class MissingReferencesFinder : MonoBehaviour {
 #if UNITY_EDITOR_OSX
         return !path.StartsWith("/");
 #else
-        //return path.Substring(1, 2) == ":\\";
-        return true;
+        return !path.Substring(1, 2) == ":\\";
 #endif
     }
 
