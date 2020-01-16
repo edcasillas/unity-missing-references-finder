889a13fd8c705191c714a08984f04ed769565ab6
Ed Casillas <eduardo_casillas@hotmail.com>
2020-01-15 21:45:32 -0600

    continue performance improvements.

--- Editor/MissingReferencesFinder.cs
+++ Editor/MissingReferencesFinder.cs
@@ -1,10 +1,16 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using UnityEditor;
 using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MissingReferencesFinder : MonoBehaviour {
+    private class ObjectData {
+        public float ExpectedProgress;
+        public GameObject GameObject;
+    }
+    
     [MenuItem("Tools/Find Missing References/In scene", false, 50)]
     public static void FindMissingReferencesInCurrentScene() {
         var scene = SceneManager.GetActiveScene();
@@ -12,7 +18,12 @@ public class MissingReferencesFinder : MonoBehaviour {
         
         var rootObjects = scene.GetRootGameObjects();
         
-        var finished = FindMissingReferences(scene.path, rootObjects, true);
+        var queue = new Queue<ObjectData>();
+        foreach (var rootObject in rootObjects) {
+            queue.Enqueue(new ObjectData{ExpectedProgress = 1/(float)rootObjects.Length, GameObject = rootObject});
+        }
+        
+        var finished = findMissingReferences(scene.path, queue, true);
         showFinishDialog(!finished);
     }
 
@@ -112,7 +123,7 @@ public class MissingReferencesFinder : MonoBehaviour {
             }
         }
     }
-    
+
     private static bool findMissingReferences(string context, GameObject go, float initialProgress, float progressThisObject, bool findInChildren = false) {
         var components = go.GetComponents<Component>();
         
@@ -160,6 +171,58 @@ public class MissingReferencesFinder : MonoBehaviour {
 
                 currentProgress += progressEachComponent;
             }
+        }
+
+        return true;
+    }
+    
+    private static bool findMissingReferences(string context, Queue<ObjectData> queue, bool findInChildren = false) {
+        var currentProgress = 0f;
+        while (queue.Any()) {
+            var data = queue.Dequeue();
+            var go = data.GameObject;
+            var components = go.GetComponents<Component>();
+        
+            float progressEachComponent;
+            if (findInChildren) {
+                progressEachComponent = (data.ExpectedProgress) / (float)(components.Length + go.transform.childCount);
+            } else {
+                progressEachComponent = data.ExpectedProgress / (float)components.Length;   
+            }
+            
+            for (var j = 0; j < components.Length; j++) {
+                currentProgress += progressEachComponent;
+                if (EditorUtility.DisplayCancelableProgressBar($"Searching missing references in {context}",
+                                                               go.name,
+                                                               currentProgress)) {
+                    return false;
+                }
+            
+                var c = components[j];
+                if (!c) {
+                    Debug.LogError("Missing Component in GO: " + FullPath(go), go);
+                    continue;
+                }
+
+                var so = new SerializedObject(c);
+                var sp = so.GetIterator();
+
+                while (sp.NextVisible(true)) {
+                    if (sp.propertyType == SerializedPropertyType.ObjectReference) {
+                        if (sp.objectReferenceValue           == null
+                         && sp.objectReferenceInstanceIDValue != 0) {
+                            ShowError(context, go, c.GetType().Name, ObjectNames.NicifyVariableName(sp.name));
+                        }
+                    }
+                }
+            }
+
+            if (findInChildren) {
+                foreach (Transform child in go.transform) {
+                    if (child.gameObject == go) continue;
+                    queue.Enqueue(new ObjectData{ExpectedProgress = progressEachComponent, GameObject = child.gameObject});
+                }
+            }   
         }
 
         return true;
