799e646e7be1820fbe3985f5d9d11af57402367a
Ed Casillas <ecasillasmarca@zynga.com>
2023-09-07 13:59:20 -0700

    V1 of `MissingReferencesResultsWindow`

--- Editor/MissingReferencesResultsWindow.cs
+++ Editor/MissingReferencesResultsWindow.cs
@@ -1,100 +1,167 @@
-using System.Collections.Generic;
-using System.Linq;
 using UnityEditor;
 using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
 
-internal class ComponentProperty {
-	public Component Component;
-	public string PropertyName;
+internal class ComponentProperty
+{
+    public Component Component;
+    public string PropertyName;
 
-	public ComponentProperty(Component component, SerializedProperty serializedProperty) {
-		Component = component;
-		PropertyName = ObjectNames.NicifyVariableName(serializedProperty.name);
-	}
+    public ComponentProperty(Component component, SerializedProperty serializedProperty)
+    {
+        Component = component;
+        PropertyName = ObjectNames.NicifyVariableName(serializedProperty.name);
+    }
 }
 
-public class MissingReferencesResultsWindow : EditorWindow {
-	private static MissingReferencesResultsWindow instance;
+public class MissingReferencesResultsWindow : EditorWindow
+{
+    private static MissingReferencesResultsWindow instance;
 
-	private readonly Dictionary<GameObject, int> missingComponents = new Dictionary<GameObject, int>();
-	private readonly Dictionary<GameObject, IList<ComponentProperty>> missingReferences = new Dictionary<GameObject, IList<ComponentProperty>>();
+    private readonly Dictionary<GameObject, int> missingComponents = new Dictionary<GameObject, int>();
+    private readonly Dictionary<GameObject, IList<ComponentProperty>> missingReferences = new Dictionary<GameObject, IList<ComponentProperty>>();
+    private bool isSearching = false;
 
-	private GameObject selectedGameObject;
-	private GameObject SelectedGameObject {
-		get => selectedGameObject;
-		set {
-			var changed = selectedGameObject != value;
-			selectedGameObject = value;
-			if(!changed) return;
+    private GameObject selectedGameObject;
+    private GameObject SelectedGameObject
+    {
+        get => selectedGameObject;
+        set
+        {
+            var changed = selectedGameObject != value;
+            selectedGameObject = value;
+            if (!changed) return;
 
-			if (!selectedGameObject) {
-				// Remove results?
-				return;
-			}
-			refreshMissingReferences();
-		}
-	}
+            if (!selectedGameObject)
+            {
+                // Remove results?
+                return;
+            }
+            refreshMissingReferences();
+        }
+    }
 
-	[MenuItem("GameObject/Find Missing References")]
-	public static void ShowWindow() {
-		if (!instance) {
-			instance              = GetWindow<MissingReferencesResultsWindow>("Find Missing References");
-		}
+    private Vector2 componentsScrollPos = Vector2.zero;
+    private Vector2 referencesScrollPos = Vector2.zero;
 
-		var selected = Selection.transforms.FirstOrDefault()?.gameObject;
-		if (instance.SelectedGameObject) {
-			instance.refreshMissingReferences();
-		} else {
-			instance.SelectedGameObject = selected;
-		}
-		instance.Show();
-	}
+    private bool showMissingComponents = false;
+    private bool showMissingReferences = false;
 
-	[MenuItem("GameObject/Find Missing References", true)]
-	private static bool ValidateOneGameObjectSelected() => (Selection.transforms?.Length ?? 0) == 1;
+    [MenuItem("GameObject/Find Missing References")]
+    public static void ShowWindow()
+    {
+        if (!instance)
+        {
+            instance = GetWindow<MissingReferencesResultsWindow>("Find Missing References");
+        }
 
-	private void OnGUI()
-	{
-		SelectedGameObject = (GameObject)EditorGUILayout.ObjectField("Finding missing references in: ", SelectedGameObject, typeof(GameObject), true);
+        var selected = Selection.transforms.FirstOrDefault()?.gameObject;
+        if (instance.SelectedGameObject)
+        {
+            instance.refreshMissingReferences();
+        }
+        else
+        {
+            instance.SelectedGameObject = selected;
+        }
+        instance.Show();
+    }
 
-		EditorGUILayout.Space();
+    [MenuItem("GameObject/Find Missing References", true)]
+    private static bool ValidateOneGameObjectSelected() => (Selection.transforms?.Length ?? 0) == 1;
 
-		EditorGUILayout.LabelField("GameObjects with Missing Components:");
-		foreach (var kvp in missingComponents)
-		{
-			EditorGUILayout.ObjectField(kvp.Key, typeof(GameObject), true);
-			EditorGUILayout.LabelField($"Missing Components: {kvp.Value}");
-			EditorGUILayout.Space();
-		}
+    private void OnGUI()
+    {
 
-		EditorGUILayout.Space();
+        SelectedGameObject = (GameObject)EditorGUILayout.ObjectField("Game Object: ", SelectedGameObject, typeof(GameObject), true);
+		if(GUILayout.Button("Refresh")) refreshMissingReferences();
 
-		EditorGUILayout.LabelField("GameObjects with Missing Component Properties:");
-		foreach (var kvp in missingReferences)
-		{
-			EditorGUILayout.ObjectField(kvp.Key, typeof(GameObject), true);
-			foreach (var property in kvp.Value)
-			{
-				EditorGUILayout.LabelField($"Component: {property.Component.GetType().Name}, Property: {property.PropertyName}");
-			}
-			EditorGUILayout.Space();
-		}
-	}
+        EditorGUILayout.Space();
 
-	private void refreshMissingReferences()
-	{
-		missingComponents.Clear();
-		missingReferences.Clear();
+        if (isSearching)
+        {
+            showLoadingSpinner($"Finding missing references in {SelectedGameObject.name}");
+            return;
+        }
 
-		if (SelectedGameObject != null)
-		{
-			EditorUtility.DisplayProgressBar("Missing References Finder", $"Searching in {SelectedGameObject.name}", 0f);
+        EditorGUILayout.BeginVertical();
 
-			MissingReferencesFinder.FindMissingReferences(SelectedGameObject, true, missingComponents, missingReferences);
+        showMissingComponents = DrawCollapsibleSection("Missing Components", missingComponents.Count, showMissingComponents);
+        if (showMissingComponents)
+        {
+            componentsScrollPos = EditorGUILayout.BeginScrollView(componentsScrollPos);
+            DrawMissingComponentsTable();
+            EditorGUILayout.EndScrollView();
+        }
 
-			EditorUtility.ClearProgressBar();
-		}
+        showMissingReferences = DrawCollapsibleSection("Missing References", missingReferences.Values.Sum(list => list.Count), showMissingReferences);
+        if (showMissingReferences)
+        {
+            referencesScrollPos = EditorGUILayout.BeginScrollView(referencesScrollPos);
+            DrawMissingReferencesTable();
+            EditorGUILayout.EndScrollView();
+        }
 
-		Repaint();
-	}
+        EditorGUILayout.EndVertical();
+    }
+
+    private bool DrawCollapsibleSection(string sectionTitle, int resultCount, bool isExpanded)
+    {
+        EditorGUILayout.BeginHorizontal();
+        isExpanded = EditorGUILayout.Foldout(isExpanded, $"{sectionTitle} ({resultCount} results)", true);
+        EditorGUILayout.EndHorizontal();
+        return isExpanded;
+    }
+
+    private void DrawMissingComponentsTable()
+    {
+        foreach (var kvp in missingComponents)
+        {
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.ObjectField(kvp.Key, typeof(GameObject), true, GUILayout.MinWidth(150f));
+            EditorGUILayout.LabelField(kvp.Value.ToString(), GUILayout.MinWidth(150f));
+            EditorGUILayout.EndHorizontal();
+            EditorGUILayout.Space();
+        }
+    }
+
+    private void DrawMissingReferencesTable()
+    {
+        foreach (var kvp in missingReferences)
+        {
+            foreach (var property in kvp.Value)
+            {
+                EditorGUILayout.BeginHorizontal();
+                EditorGUILayout.ObjectField(kvp.Key, typeof(GameObject), true, GUILayout.MinWidth(150f));
+                EditorGUILayout.LabelField(property.Component.GetType().Name, GUILayout.MinWidth(150f));
+                EditorGUILayout.LabelField(property.PropertyName, GUILayout.MinWidth(150f));
+                EditorGUILayout.EndHorizontal();
+                EditorGUILayout.Space();
+            }
+        }
+    }
+
+    private void refreshMissingReferences()
+    {
+        isSearching = true;
+        missingComponents.Clear();
+        missingReferences.Clear();
+
+        if (SelectedGameObject)
+        {
+            MissingReferencesFinder.FindMissingReferences(SelectedGameObject, true, missingComponents, missingReferences);
+        }
+
+        isSearching = false;
+        Repaint();
+    }
+
+    private void showLoadingSpinner(string label)
+    {
+        EditorGUILayout.LabelField(label, EditorStyles.boldLabel);
+        var spinnerRect = GUILayoutUtility.GetRect(EditorGUIUtility.currentViewWidth, EditorGUIUtility.singleLineHeight);
+        EditorGUI.ProgressBar(spinnerRect, -1f, string.Empty);
+        Repaint();
+    }
 }
